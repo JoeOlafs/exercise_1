b7a755dfac8f64ef7ffc86b1d14dd022bd5826b5
JoeOlafs <jaolafsson@hotmail.com>
2021-01-21 00:13:26 +0200

    Setup and Preperation

--- main.java
+++ main.java
@@ -1,1 +1,10 @@
-"#exercise_1" 
+ /****    
+ * Name: Johannes Olafsson
+ * Student Number: T00629654   
+ * Date: January 20, 2021
+ * Program Description:  include an appropriate one or more line description here
+ ****/
+
+public class Main{
+     
+}
