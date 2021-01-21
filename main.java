cf5d0538e4bb16eeb6de9678c217953f0c0f9fe8
JoeOlafs <jaolafsson@hotmail.com>
2021-01-21 09:52:37 +0200

    Setup

--- main.java
+++ main.java
@@ -6,5 +6,13 @@
  ****/
 
 public class Main{
-     
+     String [] stores = new String [7];
+     String [] months = new String [6];
+     int [] sales = new int [1]; //????
+     int [] totals = new int [1]; //????
+     String[] [] table = new String [1] [2]; //????
+
+     public static void main(String[] args){
+          System.out.println("Hello World");
+     }
 }
