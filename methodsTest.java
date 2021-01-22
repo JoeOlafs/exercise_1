dfd95bfa8c921a061cface85884c3be7c6b76d0d
JoeOlafs <jaolafsson@hotmail.com>
2021-01-23 00:33:33 +0200

    file management

--- methodsTest.java
+++
@@ -1,6 +0,0 @@
-public class methodsTest {
-     public static void Initialize(){
-          test.city = new String[] {"Chilliwack", "Kamloops", "Kelowna", "Nanaimo", "Surrey", "Vancouver", "Victoria"};
-          test.month = new String[] {"Jan", "Feb", "Mar", "Apr", "May", "June"};
-     }
-}
